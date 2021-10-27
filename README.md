245a794312422a7dca6ddb772cf9006965f58628
Johannes Bechberger <johannes.bechberger@kit.edu>
2021-10-27 18:09:19 +0200

    Add content

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # microservice-helper
-Small helper repo for microservice analyses based on jaxrs-cfx
+Small helper repo for microservice analyses based on jaxrs-cfx.
+References all packages needed to setup a server (and the Joana 
+annotations package).
+
+Idea
+----
+Encapsulate the microservice server stuff with a well-defined interface:
+We can than make assumptions on the server initialisation in the 
+component based analysis :)
+
+Usage
+-----
+Use it via maven
+
+```xml
+<dependency>
+  <groupId>io.github.joana-team</groupId>
+  <artifactId>microservice-helper</artifactId>
+  <version>1.0.3</version>
+</dependency>
+```
+
+For a minimal example of how this could be used to create a microservice with OpenAPI, 
+look no further than the
+[cat shop example](https://github.com/joana-team/joana/tree/openapi/ifc/sdg/schemas/catshop/shop).
+
+
+
+How to publish a maven package
+------------------------------
+```sh
+mvn clean   # Delete build contents                
+mvn compile # Compile classes
+mvn package # Build JAR
+mvn verify  # Will sign/check GPG
+mvn install # Install to local maven repo
+mvn deploy  # Push to Sonatype->maven central
+``
---
+++ src/edu/kit/joana/microservices/Server.java
@@ -0,0 +1,31 @@
+package edu.kit.joana.microservices;
+
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+
+public class Server {
+    /**
+     * Run Jaxrs-CFX service implementation at the passed url
+     * @param url service url, e.g. "http://localhost:9010/"
+     * @param apiServiceImpl service impl object
+     * @param resourceClasses special resource classes used by the service impl object
+     */
+    public static void run(String url, Object apiServiceImpl, Class<?>... resourceClasses) {
+        JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
+        factory.setResourceClasses(resourceClasses);
+        factory.setResourceClasses(apiServiceImpl.getClass());
+        factory.setResourceProvider(apiServiceImpl.getClass(),
+                new SingletonResourceProvider(apiServiceImpl));
+        factory.setProvider(new org.codehaus.jackson.jaxrs.JacksonJsonProvider());
+        factory.setAddress(url);
+        factory.create();
+        System.out.println("Server ready...");
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Server exiting ...");
+        System.exit(0);
+    }
+}
